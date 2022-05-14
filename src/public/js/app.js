56c00c3bf969d75a23d96b6ff59bffddbb5d9ecb
JSM000 <67637792+JSM000@users.noreply.github.com>
2022-05-14 12:59:10 +0900

    .

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,51 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+const socket = new WebSocket(`ws://${window.location.host}`);
+let nickname = "";
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  const newData = JSON.parse(message.data);
+  switch (newData.type) {
+    case "new_message":
+      const li = document.createElement("li");
+      li.innerText = `${nickname} : ${newData.payload}`;
+      messageList.append(li);
+      break;
+    case "nickname":
+      nickname = newData.payload;
+      break;
+    default:
+      console.log("일치하는 데이터가 없습니다.");
+  }
+});
+
+//서버가 꺼지면 발생
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+};
+
+const handleNickSubmit = (e) => {
+  e.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+};
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,9 +7,30 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+//http 서버를 만들고 저장해서 접근할수 있게 함. http 서버와 웹소켓 서버를 둘다 만드는 법
+const server = http.createServer(app);
+// server를 넣어주지 않으면 그냥 웹소켓 서버만 만들어짐. http서버에 웹소켓 서버를 올리는것?
+const wss = new WebSocket.Server({ server });
+
+// fake database
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Browser ✔");
+  // 사용자가 브라우져를 끄면 발생
+  socket.on("close", () => console.log("Disconnected from Browser ❌"));
+  socket.on("message", (message) => {
+    sockets.forEach((socket) => {
+      socket.send(message.toString());
+    });
+  });
+});
+
+server.listen(3000, handleListen);
