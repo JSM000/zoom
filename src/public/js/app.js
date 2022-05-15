8993763af43f5c4355ecd3c1555b90e6129e9d5e
JSM000 <dndls000@naver.com>
2022-05-15 21:46:18 +0900

    finish chatting with only ws

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,7 +2,6 @@ const messageList = document.querySelector("ul");
 const messageForm = document.querySelector("#message");
 const nickForm = document.querySelector("#nick");
 const socket = new WebSocket(`ws://${window.location.host}`);
-let nickname = "";
 
 function makeMessage(type, payload) {
   const msg = { type, payload };
@@ -14,19 +13,9 @@ socket.addEventListener("open", () => {
 });
 
 socket.addEventListener("message", (message) => {
-  const newData = JSON.parse(message.data);
-  switch (newData.type) {
-    case "new_message":
-      const li = document.createElement("li");
-      li.innerText = `${nickname} : ${newData.payload}`;
-      messageList.append(li);
-      break;
-    case "nickname":
-      nickname = newData.payload;
-      break;
-    default:
-      console.log("일치하는 데이터가 없습니다.");
-  }
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 //서버가 꺼지면 발생
@@ -38,6 +27,9 @@ const handleSubmit = (e) => {
   e.preventDefault();
   const input = messageForm.querySelector("input");
   socket.send(makeMessage("new_message", input.value));
+  const li = document.createElement("li");
+  li.innerText = `You: ${input.value}`;
+  messageList.append(li);
   input.value = "";
 };
 
@@ -45,6 +37,7 @@ const handleNickSubmit = (e) => {
   e.preventDefault();
   const input = nickForm.querySelector("input");
   socket.send(makeMessage("nickname", input.value));
+  input.value = "";
 };
 
 messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -23,13 +23,21 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("Connected to Browser ✔");
   // 사용자가 브라우져를 끄면 발생
   socket.on("close", () => console.log("Disconnected from Browser ❌"));
-  socket.on("message", (message) => {
-    sockets.forEach((socket) => {
-      socket.send(message.toString());
-    });
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((socket) => {
+          socket.send(`${socket.nickname}: ${message.payload.toString()}`);
+        });
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
   });
 });
 
