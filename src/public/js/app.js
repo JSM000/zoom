3dd843771ab371253d550567660f7a8e9987d18d
JSM000 <dndls000@naver.com>
2022-05-15 23:16:03 +0900

    .

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,44 +1,31 @@
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message");
-const nickForm = document.querySelector("#nick");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
 
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+let roomName;
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerHTML = `Room ${roomName}`;
 }
 
-socket.addEventListener("open", () => {
-  console.log("Connected to Server ✅");
-});
-
-socket.addEventListener("message", (message) => {
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  messageList.append(li);
-});
-
-//서버가 꺼지면 발생
-socket.addEventListener("close", () => {
-  console.log("Disconnected from Server ❌");
-});
-
-const handleSubmit = (e) => {
+function handleRoomSubmit(e) {
   e.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("new_message", input.value));
-  const li = document.createElement("li");
-  li.innerText = `You: ${input.value}`;
-  messageList.append(li);
+  const input = form.querySelector("input");
+  // 단순히 메세지를 전달하는 것이 아니라 "room" 이라는 이벤트를 전달함.
+  // 이벤트의 이름은 내가 마음대로 지정함.
+  // 이벤트에 딸린 데이터를 오브젝트로 전달 가능! 알아서 변환해줌
+  // 콜백을 넣으면 서버의 값을 받아서 프론트에서 실행됨! / 마지막으로 넣어줘야함 이유는 잘 모르겠음.
+  // 원하는 모든것을 여러개 넣어 줄수 있음!
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
-};
+}
 
-const handleNickSubmit = (e) => {
-  e.preventDefault();
-  const input = nickForm.querySelector("input");
-  socket.send(makeMessage("nickname", input.value));
-  input.value = "";
-};
-
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
-import WebSocket from "ws";
+import { Server } from "socket.io";
 import express from "express";
+import { SocketAddress } from "net";
 
 const app = express();
 
@@ -14,31 +15,45 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 // app.listen(3000, handleListen);
 
 //http 서버를 만들고 저장해서 접근할수 있게 함. http 서버와 웹소켓 서버를 둘다 만드는 법
-const server = http.createServer(app);
-// server를 넣어주지 않으면 그냥 웹소켓 서버만 만들어짐. http서버에 웹소켓 서버를 올리는것?
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
 
-// fake database
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anon";
-  console.log("Connected to Browser ✔");
-  // 사용자가 브라우져를 끄면 발생
-  socket.on("close", () => console.log("Disconnected from Browser ❌"));
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((socket) => {
-          socket.send(`${socket.nickname}: ${message.payload.toString()}`);
-        });
-        break;
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
+wsServer.on("connection", (socket) => {
+  // 모든 이벤트가 실행될 떄마다 호출되는 미들웨어!
+  socket.onAny((event) => {
+    console.log("Socket Event: ", event);
+  });
+  socket.on("enter_room", (roomName, showRoom) => {
+    socket.join(roomName); // 이름 지어준 방으로 소켓들을 연결시켜줌.
+    showRoom();
+    // console.log(socket.rooms); // 연결된 방들을 보여줌. 기본적으로 자신의 아이디로 이름 지어진 방에 연결되어있음.
+    // https://socket.io/docs/v4/server-api/ 다양한 API들 문서
+    // socket.leave("room1"), socket.to("room1").emit("hello")
   });
 });
 
-server.listen(3000, handleListen);
+// server를 넣어주지 않으면 그냥 웹소켓 서버만 만들어짐. http서버에 웹소켓 서버를 올리는것?
+//const wss = new WebSocket.Server({ server });
+
+// const sockets = [];
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "Anon";
+//   console.log("Connected to Browser ✔");
+//   // 사용자가 브라우져를 끄면 발생
+//   socket.on("close", () => console.log("Disconnected from Browser ❌"));
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((socket) => {
+//           socket.send(`${socket.nickname}: ${message.payload.toString()}`);
+//         });
+//         break;
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//     }
+//   });
+// });
+
+httpServer.listen(3000, handleListen);
