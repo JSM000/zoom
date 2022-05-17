313cceae6ebd661a88a65749cfa0b013e6cb29c0
JSM000 <67637792+JSM000@users.noreply.github.com>
2022-05-17 19:56:05 +0900

    finish chatting

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,23 +1,54 @@
 const socket = io();
 
 const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
+const welcomeForm = welcome.querySelector("form");
 const room = document.getElementById("room");
 
 room.hidden = true;
 
 let roomName;
 
-function showRoom() {
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+function handleMessageSubmit(e) {
+  e.preventDefault();
+  const input = room.querySelector("#msg input");
+  socket.emit("new_message", input.value, roomName, () => {
+    console.log(input.value);
+    addMessage(`You: ${input.value}`);
+    input.value = "";
+  });
+}
+
+function handleNicknameSubmit(e) {
+  e.preventDefault();
+  const input = room.querySelector("#name input");
+  socket.emit("nickname", input.value);
+}
+
+function titleChange(roomName, count) {
+  const h3 = room.querySelector("h3");
+  h3.innerHTML = `Room ${roomName} (${count})`;
+}
+
+function showRoom(newCount) {
   welcome.hidden = true;
   room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerHTML = `Room ${roomName}`;
+  titleChange(roomName, newCount);
+  const msgForm = room.querySelector("#msg");
+  const nameForm = room.querySelector("#name");
+  msgForm.addEventListener("submit", handleMessageSubmit);
+  nameForm.addEventListener("submit", handleNicknameSubmit);
 }
 
 function handleRoomSubmit(e) {
   e.preventDefault();
-  const input = form.querySelector("input");
+  const input = welcomeForm.querySelector("input");
   // 단순히 메세지를 전달하는 것이 아니라 "room" 이라는 이벤트를 전달함.
   // 이벤트의 이름은 내가 마음대로 지정함.
   // 이벤트에 딸린 데이터를 오브젝트로 전달 가능! 알아서 변환해줌
@@ -28,4 +59,29 @@ function handleRoomSubmit(e) {
   input.value = "";
 }
 
-form.addEventListener("submit", handleRoomSubmit);
+welcomeForm.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", (user, newCount) => {
+  titleChange(roomName, newCount);
+  addMessage(`${user} joined!`);
+});
+
+socket.on("bye", (user, newCount) => {
+  titleChange(roomName, newCount);
+  addMessage(`${user} left ㅠㅠ`);
+});
+
+socket.on("new_message", addMessage);
+
+socket.on("room_change", (rooms) => {
+  const roomList = welcome.querySelector("ul");
+  roomList.innerHTML = "";
+  if (rooms.length === 0) {
+    return;
+  }
+  rooms.forEach((room) => {
+    const li = document.createElement("li");
+    li.innerHTML = room;
+    roomList.appendChild(li);
+  });
+});
--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import http from "http";
 import { Server } from "socket.io";
 import express from "express";
-import { SocketAddress } from "net";
-
+import { instrument } from "@socket.io/admin-ui";
 const app = express();
 
 app.set("view engine", "pug");
@@ -14,27 +13,69 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log("Listening on http://localhost:3000");
 // app.listen(3000, handleListen);
 
-//http 서버를 만들고 저장해서 접근할수 있게 함. http 서버와 웹소켓 서버를 둘다 만드는 법
-const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer);
+const httpServer = http.createServer(app); //http 서버를 만들고 저장해서 접근할수 있게 함.
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+}); //http 서버와 웹소켓 서버를 둘다 만드는 법 / admin-ui추가
 
+instrument(wsServer, {
+  auth: false,
+});
+
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
+// https://socket.io/docs/v4/server-api/ 다양한 API들 문서
 wsServer.on("connection", (socket) => {
-  // 모든 이벤트가 실행될 떄마다 호출되는 미들웨어!
+  socket["nickname"] = "Anon";
   socket.onAny((event) => {
     console.log("Socket Event: ", event);
-  });
-  socket.on("enter_room", (roomName, showRoom) => {
+  }); // 모든 이벤트가 실행될 떄마다 호출되는 미들웨어!
+  socket.on("enter_room", (roomName, done) => {
     socket.join(roomName); // 이름 지어준 방으로 소켓들을 연결시켜줌.
-    showRoom();
-    // console.log(socket.rooms); // 연결된 방들을 보여줌. 기본적으로 자신의 아이디로 이름 지어진 방에 연결되어있음.
-    // https://socket.io/docs/v4/server-api/ 다양한 API들 문서
-    // socket.leave("room1"), socket.to("room1").emit("hello")
+    console.log(socket.rooms); // 연결된 방들을 보여줌. 기본적으로 자신의 아이디로 이름 지어진 방에 연결되어있음.
+    done(countRoom(roomName));
+    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); // 나를 제외한 방안의 모두에게 보내줌.
+    wsServer.sockets.emit("room_change", publicRooms()); // 서버에 접속한 모든 브라우저에 보냄.
+  });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+    );
+  }); // 기본으로 내장된 이벤트, 브라우져는 이미 닫았지만 아직 연결이 끊어지지 않은 그 찰나에 발생하는 이벤트 (그래서 room 정보가 살아있음)
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
+  }); // 기본으로 내장된 이벤트, 브라우져 연결이 완전히 끝어지고 난 후 발생.
+  socket.on("new_message", (message, roomName, done) => {
+    socket.to(roomName).emit("new_message", `${socket.nickname}: ${message}`); // 똑같은 이름의 이벤트 정의해도 괜찮음.
+    done();
+  });
+  socket.on("nickname", (nickname) => {
+    socket["nickname"] = nickname;
   });
 });
 
 // server를 넣어주지 않으면 그냥 웹소켓 서버만 만들어짐. http서버에 웹소켓 서버를 올리는것?
 //const wss = new WebSocket.Server({ server });
-
 // const sockets = [];
 // wss.on("connection", (socket) => {
 //   sockets.push(socket);
